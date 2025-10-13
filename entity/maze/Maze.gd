d9d9463cf52428dbed6db0d617fdc85262c6aceb
Ozelot Vanilla <ozelotvanilla@gmail.com>
2025-10-13 10:57:01 +0900

    [mirrored] refactor - Make path tagged as ID 0, and wall as 1

--- entity/maze/Maze.gd
+++ entity/maze/Maze.gd
@@ -4,9 +4,9 @@ extends TileMapLayer
 
 const black_and_white_atlas__source_id := 0
 
-const black_tile__atlas_coord := Vector2i(0, 0)
+const white_tile__atlas_coord := Vector2i(0, 0)
 
-const white_tile__atlas_coord := Vector2i(1, 0)
+const black_tile__atlas_coord := Vector2i(1, 0)
 
 const length_of_tile := 16
 

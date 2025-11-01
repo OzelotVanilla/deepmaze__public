3354f3e6787de8e5d47b2b2682bb04f9ef3f4ea9
Ozelot Vanilla <ozelotvanilla@gmail.com>
2025-11-01 16:33:08 +0900

    [mirrored] feature - Game Remaining Timer label added
    Also deleted unnecessary member variable in `HUD`.

--- scenes/maze_game/ui_element/hud/HUD.gd
+++ scenes/maze_game/ui_element/hud/HUD.gd
@@ -5,18 +5,25 @@ extends ColorRect
 
 var game_ref: MazeGame
 
-var level: int:
-    set = setLevel
-
 @onready var level_text__label: Label = $VBox/LevelAndPauseMargin/LevelAndPauseHBox/LevelText
+
+@onready var minute__label: Label = $VBox/TimeLabelHBox/MinuteLabel
+
+@onready var second__label: Label = $VBox/TimeLabelHBox/SecondLabel
+
+@onready var millisecond__label: Label = $VBox/TimeLabelHBox/MilliSecondLabel
 
 
 func setLevel(new_level: int):
     if new_level <= 0:
         return
 
-    level = new_level
-
     #TODO: May include special level (or SP level num) in the future.
     if new_level > 0:
         self.level_text__label.text = str("LEVEL ", str(new_level).pad_zeros(2))
+
+func setGameRemainingTime(time: float):
+    self.minute__label.text = str(mini(99, int(time / 60))).pad_zeros(2)
+    self.second__label.text = str(mini(99, int(fmod(time, 60)))).pad_zeros(2)
+    var number_after_decimal_point := time - int(time)
+    self.millisecond__label.text = str(number_after_decimal_point).pad_decimals(2).substr(2)

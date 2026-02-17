fdd88443230643d46b5fd5bc4fdf8c3b26c54350
Ozelot Vanilla <ozelotvanilla@gmail.com>
2026-02-17 18:46:47 +0900

    [mirrored] struct - Change `switchTo` series to push/pop/change, and renamed `main_scene__ref` to `root_scene__ref`
    `push/pop/change` helps better scene management, enabling stacking.

--- scenes/root/SceneRoot.gd
+++ scenes/root/SceneRoot.gd
@@ -4,36 +4,69 @@ extends Control
 ##
 ## Method start with "switch" will switch to an existed scene.
 
+
 func _ready() -> void: self.__onReady__()
 
 
-var current_scene: Control
+const scene__dict: Dictionary[StringName, PackedScene] = {
+    "maze_game": preload("res://scenes/maze_game/MazeGame.tscn"),
+    "press_any_key_title": preload("res://scenes/press_any_key_title/GamePressAnyKeyPage.tscn"),
+    "title": preload("res://scenes/title/GameTitlePage.tscn"),
+    "menu": preload("res://scenes/menu/GameMenuPage.tscn")
+}
 
+
+## Scene to be shown on the screen.
+var current_scene: Control:
+    set(new_scene):
+        ## The `CurrentScene` node in tree that display the scene.
+        var node = $CurrentScene
+        for c in node.get_children(): node.remove_child(c)
+        node.add_child(new_scene)
+
+        current_scene = new_scene
+
+## Scene that is stacked. The last element will be the current scene.
+var scene_stack: Array[BaseGameScene] = []
+
+
+## Push a new scene into the stack.
+## The scene pushed before will be alived.
+func pushScene(scene_name: StringName):
+    var scene: BaseGameScene = self.scene__dict[scene_name].instantiate()
+    self.scene_stack.push_back(scene)
+    self.current_scene = scene
+
+## Pop a scene from stack.
+## If no enough scene to be pop-ed, an error generates.
+func popScene():
+    if scene_stack.size() <= 1:
+        printerr(str(
+            "Cannot pop anymore because the scene stack is empty,",
+            " or only contains one last scene."
+        ))
+
+    var poped_scene: BaseGameScene = self.scene_stack.pop_back()
+    poped_scene.queue_free.call_deferred()
+
+    self.current_scene = self.scene_stack.back()
+
+## Change the active scene to a new scene.
+func changeScene(scene_name: StringName):
+    var scene: BaseGameScene = self.scene__dict[scene_name].instantiate()
+
+    var poped_scene: BaseGameScene = self.scene_stack.pop_back()
+    poped_scene.queue_free.call_deferred()
+
+    self.scene_stack.push_back(scene)
+    self.current_scene = scene
 
 func __onReady__():
     #TODO: Just for test.
-    #self.switchToMazeGame()
+    #self.pushScene("maze_game")
 
     # Should be:
-    self.switchToPressAnyKeyPage()
+    self.pushScene("press_any_key_title")
 
 func quitGame():
     self.get_tree().quit()
-
-func changeSceneToFile(tscn_file: PackedScene, ...postInit__args):
-    if self.current_scene != null:
-        self.current_scene.queue_free()
-
-    var scene: BaseGameScene = tscn_file.instantiate()
-    scene.postInit(postInit__args)
-    self.add_child(scene)
-    self.current_scene = scene
-
-func switchToMazeGame():
-    self.changeSceneToFile(preload("res://scenes/maze_game/MazeGame.tscn"))
-
-func switchToPressAnyKeyPage():
-    self.changeSceneToFile(preload("res://scenes/press_any_key_title/GamePressAnyKeyPage.tscn"))
-
-func switchToTitle():
-    self.changeSceneToFile(preload("res://scenes/title/GameTitlePage.tscn"))

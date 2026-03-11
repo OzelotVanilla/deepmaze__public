002b60415bb207d4bf5b948761ce8c965c2fa91b
Ozelot Vanilla <ozelotvanilla@gmail.com>
2026-03-11 17:59:06 +0900

    [mirrored] fix - Wrong alignment behaviour of `DottedBgButton`
    Also add `@tool` to it so it is visible in Godot UI Editor.
    
    (Unique ID of `HUDRightPanel` added because project's reload and regeneration)
    Co-author-by: nobu <nobuisbaobao@gmail.com>

--- ui_component/button/dotted_bg_button/DottedBgButton.gd
+++ ui_component/button/dotted_bg_button/DottedBgButton.gd
@@ -1,8 +1,14 @@
+@tool
 @icon("res://.godot/editor/icons/Button.svg")
 class_name DottedBgButton
 extends Button
 
 
+const theme_variation__normal_name := StringName("DottedBgButton_Normal")
+
+const theme_variation__focus_name := StringName("DottedBgButton_Focus")
+
+#region StyleBox used.
 const normal_stylebox = preload(
     "res://ui_component/button/dotted_bg_button/DottedBgButton_normal_stylebox.tres"
 )
@@ -17,6 +23,7 @@ const focus_stylebox = preload(
 )
 
 const font = preload("res://assets/fonts/ark-pixel-12px-monospaced-latin.otf")
+#endregion
 
 
 var should_show_focus_style: bool = false:
@@ -27,26 +34,11 @@ var should_show_focus_style: bool = false:
 
 
 func refreshHoverStylebox():
-    self.remove_theme_stylebox_override("normal")
-    self.remove_theme_stylebox_override("hover")
-    self.remove_theme_color_override("font_hover_color")
-
-    # Check if should show "focused" stylebox.
-    if self.should_show_focus_style:
-        self.add_theme_stylebox_override("normal", DottedBgButton.focus_stylebox)
-        self.add_theme_stylebox_override("hover", DottedBgButton.focus_stylebox)
-        self.add_theme_color_override("font_hover_color", Color("#000000"))
-        return
+    # Check focus state, and whether should show "focused" stylebox, and give proper stylebox.
+    if self.should_show_focus_style or self.has_focus():
+        self.theme_type_variation = DottedBgButton.theme_variation__focus_name
     else:
-        self.add_theme_stylebox_override("normal", DottedBgButton.normal_stylebox)
-
-    # Check focus state and give proper stylebox.
-    if self.has_focus():
-        self.add_theme_stylebox_override("hover", DottedBgButton.focus_stylebox)
-        self.add_theme_color_override("font_hover_color", Color("#000000"))
-    else:
-        self.add_theme_stylebox_override("hover", DottedBgButton.normal_stylebox)
-        self.add_theme_color_override("font_hover_color", Color("#000000"))
+        self.theme_type_variation = DottedBgButton.theme_variation__normal_name
 
 func on_mouse_entered():
     if not self.disabled:
@@ -63,38 +55,8 @@ func on_focus_exited():
     self.refreshHoverStylebox()
 
 func _ready() -> void:
-    # Container sizing.
-    self.size_flags_horizontal = Control.SIZE_SHRINK_BEGIN
-
-    # Text behaviour.
-    self.alignment = HorizontalAlignment.HORIZONTAL_ALIGNMENT_LEFT
-
-    # Font.
-    self.add_theme_font_override(
-        "font",
-        DottedBgButton.font
-    )
-    self.add_theme_font_size_override(
-        "font_size",
-        32
-    )
-    self.add_theme_color_override("font_color",         Color("#000000"))
-    self.add_theme_color_override("font_focus_color",   Color("#000000"))
-    self.add_theme_color_override("font_pressed_color", Color("#000000"))
-
-    # Stylebox.
-    self.add_theme_stylebox_override(
-        "normal",
-        DottedBgButton.normal_stylebox
-    )
-    self.add_theme_stylebox_override(
-        "pressed",
-        DottedBgButton.pressed_stylebox
-    )
-    self.add_theme_stylebox_override(
-        "focus",
-        DottedBgButton.focus_stylebox
-    )
+    # Default theme type variation.
+    self.theme_type_variation = DottedBgButton.theme_variation__normal_name
 
     # Signal.
     if not self.mouse_entered.is_connected(self.on_mouse_entered):
